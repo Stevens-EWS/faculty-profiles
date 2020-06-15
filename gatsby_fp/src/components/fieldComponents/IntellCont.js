c3a23190e03aeed413a71aa191e7b8e3985d77c3
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-06-15 12:25:47 -0400

    Created SubHeading Component with new formatting from DM

--- gatsby_fp/src/components/fieldComponents/IntellCont.js
+++ gatsby_fp/src/components/fieldComponents/IntellCont.js
@@ -1,112 +1,160 @@
 import React from "react"
-const shortid = require("shortid")
+import SubHeading from "./intellContComponents/SubHeading.js"
 
 export default function IntellCont({ intellContList }) {
+  const existingContypes = {
+      book: false,
+      bookChapter: false,
+      bookReview: false,
+      case: false,
+      conferenceProceeding: false,
+      instructionalMaterial: false,
+      journalArticle: false,
+      magTradePub: false,
+      newsletter: false,
+      newspaperArticle: false,
+      software: false,
+      technicalReport: false,
+      textbook: false,
+      other: false
+  }
+  intellContList.forEach(element => {
+    switch(element.contype){
+      case "Book":
+        existingContypes.book = true;
+        break;
+      case "Book Chapter":
+        existingContypes.bookChapter = true;
+        break;
+      case "Book Review":
+        existingContypes.bookReview = true;
+        break;
+      case "Case":
+        existingContypes.case = true;
+        break;
+      case "Conference Proceeding":
+        existingContypes.conferenceProceeding = true;
+        break;
+      case "Instructional Material":
+        existingContypes.instructionalMaterial = true;
+        break;
+      case "Journal Article":
+        existingContypes.journalArticle = true;
+        break;
+      case "Magazine/Trade Publication":
+        existingContypes.magTradePub = true;
+        break;
+      case "Newsletter":
+        existingContypes.newsletter = true;
+        break;
+      case "Newspaper Article":
+        existingContypes.newspaperArticle = true;
+        break;
+      case "Newspaper Article":
+        existingContypes.software = true;
+        break;
+      case "Technical Report":
+        existingContypes.technicalReport = true;
+        break;
+      case "Textbook":
+        existingContypes.textbook = true;
+        break;
+      case "Other":
+        existingContypes.other = true;
+        break;
+      default:
+        break;
+    }
+  })
+
   return (
     <>
       <div className="sectiontitle">Selected Publications</div>
       <div>
-        <ol>
-          {intellContList.map(element => (
-            <li
-              key={shortid.generate()}
-              dangerouslySetInnerHTML={{
-                __html: ` 
-                  ${
-                    element.intellcont_auth.fname
-                      ? `${element.intellcont_auth.fname}`
-                      : ``
-                  }
-                  ${
-                    element.intellcont_auth.lname
-                      ? `${element.intellcont_auth.lname}`
-                      : ``
-                  } 
-                  ${
-                    element.dty_pub
-                      ? `(${element.dty_pub}).`
-                      : ``
-                  }
-                  ${
-                    element.contype
-                      ? `${element.contype}`
-                      : ``
-                  }
-                  ${
-                    element.contypeother
-                      ? `${element.contypeother}`
-                      : ``
-                  }
-                  ${
-                    element.status
-                      ? `${element.status}`
-                      : ``
-                  }
-                  ${
-                    element.journal_name
-                      ? `${element.journal_name}`
-                      : ``
-                  }
-                  ${
-                    element.pagenum
-                      ? `${element.pagenum}`
-                      : ``
-                  }
-                  ${
-                    element.title
-                      ? `${element.title}`
-                      : ``
-                  }
-                  ${
-                    element.title_secondary
-                      ? `${element.title_secondary}`
-                      : ``
-                  }
-                  ${
-                    element.volume
-                      ? `${element.volume}`
-                      : ``
-                  }
-                  ${
-                    element.publisher
-                      ? `${element.publisher}`
-                      : ``
-                  }
-                  ${
-                    element.pubctyst
-                      ? `${element.pubctyst}`
-                      : ``
-                  }
-                  ${
-                    element.issue
-                      ? `${element.issue}`
-                      : ``
-                  }
-                  ${
-                    element.dty_acc
-                      ? `${element.dty_acc}`
-                      : ``
-                  }
-                  ${
-                    element.dty_sub
-                      ? `${element.dty_sub}`
-                      : ``
-                  }
-                  ${
-                    element.web_address
-                      ? `${element.web_address}`
-                      : ``
-                  }
-                  ${
-                    element.intellcont_auth.faculty_name
-                      ? `${element.intellcont_auth.faculty_name}`
-                      : ``
-                  }                 
-              `,
-              }}
-            ></li>
-          ))}
-        </ol>
+        {existingContypes.book &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Book"
+          />
+        }
+        {existingContypes.bookChapter &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Book Chapter"
+          />
+        }
+        {existingContypes.bookReview &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Book Review"
+          />
+        }
+        {existingContypes.case &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Case"
+          />
+        }
+        {existingContypes.conferenceProceeding &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Conference Proceeding"
+          />
+        }
+        {existingContypes.instructionalMaterial &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Instructional Material"
+          />
+        }
+        {existingContypes.journalArticle &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Journal Article"
+          />
+        }
+        {existingContypes.magTradePub &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Magazine/Trade Publication"
+          />
+        }
+        {existingContypes.newsletter &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Newsletter"
+          />
+        }
+        {existingContypes.newspaperArticle &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Newspaper Article"
+          />
+        }
+        {existingContypes.software &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Software"
+          />
+        }
+        {existingContypes.technicalReport &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Technical Report"
+          />
+        }
+        {existingContypes.textbook &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Textbook"
+          />
+        }
+        {existingContypes.other &&
+          <SubHeading 
+            intellContList={intellContList}
+            publicationType="Other"
+          />
+        }
       </div>
     </>
   )
---
+++ gatsby_fp/src/components/fieldComponents/intellContComponents/SubHeading.js
@@ -0,0 +1,104 @@
+import React from "react"
+const shortid = require("shortid")
+
+export default function IntellCont({ intellContList, publicationType }) {
+
+    return (
+    <>
+      <div className="publicationtitle">{publicationType}</div>
+        <ol>
+          {intellContList.filter(element => element.contype === publicationType).map(element => (
+            <li
+              key={shortid.generate()}
+              dangerouslySetInnerHTML={{
+                __html: ` 
+                 ${
+                    element.status
+                      ? `${element.status}`
+                      : ``
+                  }
+                  ${
+                    element.contype
+                      ? `${element.contype} -`
+                      : ``
+                  }
+                  ${
+                    element.intellcont_auth.lname
+                      ? `${element.intellcont_auth.lname},`
+                      : ``
+                  }
+                  ${
+                    element.intellcont_auth.fname
+                      ? `${element.intellcont_auth.fname}.`
+                      : ``
+                  }
+                  (2020).
+                  ${
+                    element.title
+                      ? `${element.title}.`
+                      : ``
+                  }
+                  ${
+                    element.title_secondary
+                      ? `${element.title_secondary}`
+                      : ``
+                  }
+                  ${
+                    (element.issue && !element.pagenum && !element.volume)
+                      ? `(${element.issue} ed.).`
+                      : ``
+                  }
+                  ${
+                    (!element.issue && element.pagenum && !element.volume)
+                      ? `(pp. ${element.pagenum}).`
+                      : ``
+                  }
+                  ${
+                    (!element.issue && !element.pagenum && element.volume)
+                      ? `(vol. ${element.volume}).`
+                      : ``
+                  }
+                  ${
+                    (!element.issue && element.pagenum && element.volume)
+                      ? `(vol. ${element.volume}, pp. ${element.pagenum}).`
+                      : ``
+                  }
+                  ${
+                    (element.issue && !element.pagenum && element.volume)
+                      ? `(${element.issue} ed., vol. ${element.volume}).`
+                      : ``
+                  }
+                  ${
+                    (element.issue && element.pagenum && !element.volume)
+                      ? `(${element.issue} ed., pp. ${element.pagenum}).`
+                      : ``
+                  }
+                  ${
+                    (element.issue && element.pagenum && element.volume)
+                      ? `(${element.issue} ed., vol. ${element.volume}, pp. ${element.pagenum}).`
+                      : ``
+                  }
+                  ${
+                    element.pubctyst
+                      ? `${element.pubctyst}:`
+                      : ``
+                  }
+                  ${
+                    element.publisher
+                      ? `${element.publisher}.`
+                      : ``
+                  }
+                  <br>
+                  ${
+                    element.web_address
+                      ? `${element.web_address}`
+                      : ``
+                  }                
+              `,
+              }}
+            ></li>
+          ))}
+        </ol>
+    </>
+  )
+}
