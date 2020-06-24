ab28e91ad6db9254974cfd340859f4117cc17023
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-06-24 16:16:00 -0400

    EK-136: Removed unused imports

--- gatsby_fp/src/pages/404.js
+++ gatsby_fp/src/pages/404.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
 import Helmet from "react-helmet"
 import Header from "../components/Header"
 
