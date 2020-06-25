1ba71c74fe7911be5d210f961d5054f30b344fbc
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-06-25 13:52:24 -0400

    EK-136: Removed Header import statement

--- gatsby_fp/src/pages/404.js
+++ gatsby_fp/src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Head from "../components/Layout/Head"
-import Header from "../components/Layout/Body/Header"
 import Body from "../components/Layout/Body"
 
 const NotFoundPage = () => (
