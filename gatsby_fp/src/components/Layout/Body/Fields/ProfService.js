527deb2baded20529248003dd7024bd115de0924
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-07-22 13:52:49 -0400

    EK-147: Converted ternary operations to if statements in ProfService

--- gatsby_fp/src/components/Layout/Body/Fields/ProfService.js
+++ gatsby_fp/src/components/Layout/Body/Fields/ProfService.js
@@ -2,27 +2,30 @@ import React from "react"
 const shortid = require("shortid")
 
 export default function ProfService({ profServiceList }) {
+  
+  const liInnerHTMLList =
+  profServiceList.map(element => { 
+    let liString = '';
+    if(element.org){
+      liString += `${element.org} `
+    }
+    if(element.title){
+      liString += `${element.title}`
+    }
+
+    return liString
+  })
+
   return (
     <>
       <div className="sectiontitle">Professional Service</div>
       <div>
         <ul>
-          {profServiceList.map(element => (
+        {liInnerHTMLList.map(element => (
             <li
               key={shortid.generate()}
               dangerouslySetInnerHTML={{
-                __html: ` 
-                ${
-                  element.org
-                    ? `${element.org}`
-                    : ``
-                } 
-                ${
-                  element.title
-                    ? `${element.title}`
-                    : ``
-                }
-            `,
+                __html: element
               }}
             ></li>
           ))}
