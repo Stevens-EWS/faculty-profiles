23c1685c192754f2664b424f28a8a87f9e77422f
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-07-22 13:44:08 -0400

    EK-147: Removed uneccessary indentation in Education component

--- gatsby_fp/src/components/Layout/Body/Fields/Education.js
+++ gatsby_fp/src/components/Layout/Body/Fields/Education.js
@@ -5,25 +5,25 @@ export default function Education({ educationList }) {
 
   const liInnerHTMLList =
   educationList.map(element => { 
-      let liString = '';
-      if(element.deg  && !element.degother){
-        liString += `${element.deg} `
-      }
-      if(element.degother){
-        liString += `${element.degother} `
-      }
-      if(element.dty_comp){
-        liString += `(${element.dty_comp}) `
-      }
-      if(element.school){
-        liString += `${element.school} `
-      }         
-      if(element.major){
-        liString += `(${element.major}) `
-      }
+    let liString = '';
+    if(element.deg  && !element.degother){
+      liString += `${element.deg} `
+    }
+    if(element.degother){
+      liString += `${element.degother} `
+    }
+    if(element.dty_comp){
+      liString += `(${element.dty_comp}) `
+    }
+    if(element.school){
+      liString += `${element.school} `
+    }         
+    if(element.major){
+      liString += `(${element.major}) `
+    }
 
-      return liString
-    })
+    return liString
+  })
 
   return (
     <>
