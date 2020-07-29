5c6773c8c33cc0fd84baba79c5f6b75e2e13d8d4
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-07-29 14:47:36 -0400

    EK-147: Get rid of awkward space using flex box

--- gatsby_fp/src/components/Layout/Body/Fields/GenInfoTable.js
+++ gatsby_fp/src/components/Layout/Body/Fields/GenInfoTable.js
@@ -17,109 +17,85 @@ export default function GenInfoTable({
 }) {
   return (
     <>
-      <div className="table_wrapper">
-        <div className="table_wrapper_inner">
-          <div 
-            className="title"
-            valign="top"
-            dangerouslySetInnerHTML={{ __html: title }}
-          ></div>
-          <div className="img_container">
-            <img
-              src={`data:image/jpeg;base64,${imgsrc}`}
-              className="bioimage"
-              alt={`${firstName} ${lastName}`}
-            />
-          </div>
-          <div className="table_wrapper">
-            <div className="table_wrapper_inner">
-              <div className = "pf_field_wrapper">
-                <table cellPadding="0" cellSpacing="0" border="0">
-                  <tbody>
-                    {school && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `School: ${school}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {department && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Department: ${department}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {building && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Building: ${building}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {room && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Room: ${room}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {phone && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Phone: ${phone}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {fax && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Fax: ${fax}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {email && (
-                      <tr>
-                        <td
-                          valign="top"
-                          dangerouslySetInnerHTML={{
-                            __html: `Email: ${email}`,
-                          }}
-                        ></td>
-                      </tr>
-                    )}
-                    {website && (
-                      <tr>
-                        <td>
-                          <a href={website} target="_blank" rel="noreferrer">
-                            Website
-                          </a>
-                        </td>
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+      <div className="flex_box">
+        <div className="img_container">
+          <img
+            src={`data:image/jpeg;base64,${imgsrc}`}
+            className="bioimage"
+            alt={`${firstName} ${lastName}`}
+          />
+        </div>
+        <div className = "pf_field_wrapper">
+          {title && (
+            <p
+              className="title"
+              valign="top"
+              dangerouslySetInnerHTML={{ __html: title }}
+            ></p>
+          )} 
+          {school && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `School: ${school}`,
+              }}
+            ></p>
+          )}
+          {department && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Department: ${department}`,
+              }}
+            ></p>
+          )}
+          {building && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Building: ${building}`,
+              }}
+            ></p>
+          )}
+          {room && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Room: ${room}`,
+              }}
+            ></p>
+          )}
+          {phone && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Phone: ${phone}`,
+              }}
+            ></p>
+          )}
+          {fax && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Fax: ${fax}`,
+              }}
+            ></p>
+          )}
+          {email && (
+            <p
+              valign="top"
+              dangerouslySetInnerHTML={{
+                __html: `Email: ${email}`,
+              }}
+            ></p>
+          )}
+          {website && (
+            <p>
+              <a href={website} target="_blank" rel="noreferrer">
+                Website
+              </a>
+            </p>
+          )}
         </div>
       </div>
     </>
