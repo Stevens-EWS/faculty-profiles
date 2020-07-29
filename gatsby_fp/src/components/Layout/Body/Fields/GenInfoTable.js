f2b16fbb4c9f77be7815a9e6e19fdb4679a1786d
cs-paz <32683410+cs-paz@users.noreply.github.com>
2020-07-29 10:47:12 -0400

    EK-139: Change CSS and add wrappers to display pf fields under faculty img on phones

--- gatsby_fp/src/components/Layout/Body/Fields/GenInfoTable.js
+++ gatsby_fp/src/components/Layout/Body/Fields/GenInfoTable.js
@@ -19,120 +19,107 @@ export default function GenInfoTable({
     <>
       <div className="table_wrapper">
         <div className="table_wrapper_inner">
-          <table
-            cellPadding="5"
-            cellSpacing="0"
-            border="0"
-            className="bioregion"
-            width="100%"
-          >
-            <tbody>
-              <tr>
-                <td valign="top" align="left" width="160px">
-                  <img
-                    src={`data:image/jpeg;base64,${imgsrc}`}
-                    className="bioimage"
-                    alt={`${firstName} ${lastName}`}
-                  />
-                </td>
-                <td valign="top" align="left">
-                  <div className="table_wrapper">
-                    <div className="table_wrapper_inner">
-                      <table cellPadding="0" cellSpacing="0" border="0">
-                        <tbody>
-                          <tr>
-                            <td
-                              valign="top"
-                              dangerouslySetInnerHTML={{ __html: title }}
-                            ></td>
-                          </tr>
-                          {school && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `School: ${school}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {department && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Department: ${department}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {building && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Building: ${building}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {room && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Room: ${room}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {phone && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Phone: ${phone}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {fax && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Fax: ${fax}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {email && (
-                            <tr>
-                              <td
-                                valign="top"
-                                dangerouslySetInnerHTML={{
-                                  __html: `Email: ${email}`,
-                                }}
-                              ></td>
-                            </tr>
-                          )}
-                          {website && (
-                            <tr>
-                              <td>
-                                <a href={website} target="_blank" rel="noreferrer">
-                                  Website
-                                </a>
-                              </td>
-                            </tr>
-                          )}
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div 
+            className="title"
+            valign="top"
+            dangerouslySetInnerHTML={{ __html: title }}
+          ></div>
+          <div className="img_container">
+            <img
+              src={`data:image/jpeg;base64,${imgsrc}`}
+              className="bioimage"
+              alt={`${firstName} ${lastName}`}
+            />
+          </div>
+          <div className="table_wrapper">
+            <div className="table_wrapper_inner">
+              <div className = "pf_field_wrapper">
+                <table cellPadding="0" cellSpacing="0" border="0">
+                  <tbody>
+                    {school && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `School: ${school}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {department && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Department: ${department}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {building && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Building: ${building}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {room && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Room: ${room}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {phone && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Phone: ${phone}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {fax && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Fax: ${fax}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {email && (
+                      <tr>
+                        <td
+                          valign="top"
+                          dangerouslySetInnerHTML={{
+                            __html: `Email: ${email}`,
+                          }}
+                        ></td>
+                      </tr>
+                    )}
+                    {website && (
+                      <tr>
+                        <td>
+                          <a href={website} target="_blank" rel="noreferrer">
+                            Website
+                          </a>
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
