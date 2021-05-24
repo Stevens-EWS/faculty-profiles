1ec0a96474162709b1ad46cc9268ff7a03822454
webutil <webutil-git@stevens.edu>
2021-05-24 13:00:34 -0400

    Scheduled build.

--- gatsby_fp/build_cron.sh
+++ gatsby_fp/build_cron.sh
@@ -1,6 +1,6 @@
+cd /usr/local/bin/faculty-profiles/gatsby_fp/
 git merge master Scheduled-builds
 date +%s > deploy.current
 git add -A
 git commit -m "Scheduled build."
 git push origin Scheduled-builds
-
