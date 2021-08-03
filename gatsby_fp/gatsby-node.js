82cb04656fceac2a787f71ecc8aa2ccf0f084d70
Diaeddin Motan <dmotan@macIT009028.local>
2021-08-03 14:12:51 -0400

    EK-428: Removed depricated auth option and added auth to the url directly instead.

--- gatsby_fp/gatsby-node.js
+++ gatsby_fp/gatsby-node.js
@@ -15,19 +15,18 @@ exports.onCreateNode = async ({
   node,
   actions: { createNode },
   store,
-  getCache, 
+  getCache,
   createNodeId
 }) => {
   try {
     if (node.internal.type === "profiles" && node.photo_url) {
       const urlObject = new URL(node.photo_url)
       let fileNode = await createRemoteFileNode({
-        url: `https://${urlObject.host}${urlObject.pathname}${urlObject.search}`,
+        url: `https://${urlObject.username}:${urlObject.password}@${urlObject.host}${urlObject.pathname}${urlObject.search}`,
         parentNodeId: node.id,
         getCache, // Gatsby's cache
         createNode, // helper function in gatsby-node to generate the node
         createNodeId,
-        auth: { htaccess_user: urlObject.username, htaccess_pass: urlObject.password },
         store, // Gatsby's Redux store
       })
 
@@ -40,13 +39,13 @@ exports.onCreateNode = async ({
       // err returns a string value with no ability to
       // get the status code without searching for it within the string
       (async function getStatusCodeUsingFetch() {
-        const response = await fetch(node.photo_url) 
+        const response = await fetch(node.photo_url)
         if(response.status != 404 ){
           throw new Error(`Invalid Status Code [${response.status}] for photo_url of user ${node.pf_username}`)
         }
       })()
   }
-  
+
 }
 
 exports.createPages = async ({ graphql, actions }) => {
